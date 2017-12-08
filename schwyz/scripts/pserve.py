f5b92ee61296a8dbda2e1e709cbe105f1635c0df
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-12-08 17:18:13 +0900

    Move import in method
    This `pserve` is not used in produciton. See comment.

--- schwyz/scripts/pserve.py
+++ schwyz/scripts/pserve.py
@@ -1,12 +1,16 @@
 import sys
 
-from pyramid.scripts.pserve import PServeCommand
-
 from schwyz.env import load_dotenv_vars
 
 
 def main(argv=None, quiet=False):
     """Runs original pserve with .env support."""
+    # NOTE:
+    # `pserve` (PServeCommand) needs `hupper`, it has dependency to **fcntl**.
+    # In some environment (e.g. appengine), fcntl is not found :'(
+    # So, that's why this import is in method.
+    from pyramid.scripts.pserve import PServeCommand
+
     if not argv:
         argv = sys.argv
     load_dotenv_vars()
